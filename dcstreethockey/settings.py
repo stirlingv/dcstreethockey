22aef1d51af47e0b4f6cc1dd15a0a8ef9044a910
Stirling Van Winkle <stirling@Stirlings-MacBook-Pro.local>
2016-08-21 22:50:03 -0400

    adding db tables for polls app

--- dcstreethockey/settings.py
+++ dcstreethockey/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['www.dcstreethockey.com']
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
