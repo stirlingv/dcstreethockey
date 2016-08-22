b1d6d1dbbfd8994173f1046bac21b9fbc4926418
Stirling Van Winkle <stirling@Stirlings-MacBook-Pro.local>
2016-08-21 20:07:34 -0400

    Switched db backend to sqlite and added clsses to the polls app

--- dcstreethockey/settings.py
+++ dcstreethockey/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'dcstreethockey.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'dcstreethockey',
-        'USER': 'username',
-        'PASSWORD': 'password',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
