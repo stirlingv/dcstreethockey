f45670c80095b0a20f6c24205e5b7c7675176ea3
Stirling Van Winkle <stirlingv@gmail.com>
2016-08-25 15:18:30 -0400

    adding core project for website wide html

--- polls/views.py
+++ polls/views.py
@@ -1,15 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 from .models import Question
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     context = {'latest_question_list': latest_question_list}
+#     return render(request, 'polls/index.html', context)
+
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     template = loader.get_template('polls/index.html')
+#     context = {
+#         'latest_question_list': latest_question_list,
+#     }
+#     return HttpResponse(template.render(context, request))
 
 def index(request):
-	return HttpResponse("DC Street Hockey poll index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    ques_list = [q.question_text for q in latest_question_list]
+    return render(request, "polls/index.html", context={"latest_question_list": ques_list})
+    #return HttpResponse(output)
+
+# def index(request):
+# 	#return HttpResponse("DC Street Hockey poll index.")
+# 	return render(request, "polls/index.html", context={"latest_question_list": ["question1", "question2"]})
+
 # Create your views here.
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
