0a82da9ac5c7b7115defe8acfa69ce4096e96e8e
Stirling Van Winkle <stirlingv@gmail.com>
2017-02-22 17:10:58 -0500

    adding tables to models.py file

--- leagues/models.py
+++ leagues/models.py
@@ -2,4 +2,35 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Player(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+    user_name = models.CharField(max_length=30)
+    password = models.CharField(max_length=50)
+    photo = models.ImageField()
+
+class Team(models.Model):
+    team_name = models.CharField(max_length=30)
+    team_color = models.CharField(max_length=30)
+    is_active = models.BooleanField()
+
+class Roster(models.Model):
+	player = models.ForeignKey(Player, on_delete=models.CASCADE)
+	team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    position1 = models.CharField(max_length=30)
+    position2 = models.CharField(max_length=30)
+
+ class Season(models.Model):
+	team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    is_champion = models.NullBooleanField()
+
+class league(models.Model):
+	season = models.ForeignKey(Season)
+	team = models.ForeignKey(Team)
+	division = models.PositiveSmallIntegerField()
+	win = models..PositiveSmallIntegerField()
+	loss = models.PositiveSmallIntegerField()
+	tie = models.PositiveSmallIntegerField()
+	goals_for = models.PositiveSmallIntegerField()
+	goals_against = models.PositiveSmallIntegerField()
