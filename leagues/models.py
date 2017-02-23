f691df8315a3051dc1505d817d9f1c8f2fed9484
Stirling Van Winkle <stirlingv@gmail.com>
2017-02-23 17:14:20 -0500

    adding more tables

--- leagues/models.py
+++ leagues/models.py
@@ -21,16 +21,41 @@ class Roster(models.Model):
     position1 = models.CharField(max_length=30)
     position2 = models.CharField(max_length=30)
 
- class Season(models.Model):
+class Season(models.Model):
 	team = models.ForeignKey(Team, on_delete=models.CASCADE)
     is_champion = models.NullBooleanField()
 
-class league(models.Model):
+class League(models.Model):
 	season = models.ForeignKey(Season)
 	team = models.ForeignKey(Team)
 	division = models.PositiveSmallIntegerField()
-	win = models..PositiveSmallIntegerField()
+	win = models.PositiveSmallIntegerField()
 	loss = models.PositiveSmallIntegerField()
 	tie = models.PositiveSmallIntegerField()
 	goals_for = models.PositiveSmallIntegerField()
 	goals_against = models.PositiveSmallIntegerField()
+
+class Games(models.Model):
+	season = models.ForeignKey(Season)
+	date = models.DateField()
+	time = models.TimeField()
+	awayteam = models.ForeignKey(Team)
+	hometeam = models.ForeignKey(Team)
+	ref1 = models.CharField(max_length=50)
+	ref2 = models.CharField(max_length=50)
+	notes = models.CharField(max_length=500)
+	is_regularseasion = models.BooleanField()
+
+
+class Stats(models.Model):
+	season = models.ForeignKey(Season)
+	player = models.ForeignKey(Player, on_delete=models.CASCADE)
+	game = models.ForeignKey(Games)
+	assists = models.PositiveSmallIntegerField()
+	goals_against = models.PositiveSmallIntegerField()
+	en = models.PositiveSmallIntegerField()
+
+class Refs(models.Model):
+	first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    plaer = models.ForeignKey(Player)
