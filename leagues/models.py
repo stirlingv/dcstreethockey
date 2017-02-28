b36319bec0ae55a28ff8a69f4b62db67dc7afcff
Stirling Van Winkle <stirling@Stirlings-MacBook-Pro.local>
2017-02-28 07:52:47 -0500

    changing ref def

--- leagues/models.py
+++ leagues/models.py
@@ -105,6 +105,6 @@ class Ref(models.Model):
     player = models.ForeignKey(Player)
     
     def __unicode__(self): 
-        return "%s, %s" % (self.player_last_name, self.player_first_name)
+        return "%s, %s" % (self.last_name, self.first_name)
 
 
