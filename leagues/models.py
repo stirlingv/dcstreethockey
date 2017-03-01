8ce8ed59d144ed6afa1ca1198a6b8693c35a3fff
Stirling Van Winkle <stirlingv@gmail.com>
2017-03-01 16:15:16 -0500

    modifying data model

--- leagues/models.py
+++ leagues/models.py
@@ -3,6 +3,11 @@ from __future__ import unicode_literals
 from django.db import models
 import datetime
 
+    
+YEAR_CHOICES = []
+for r in range(1980, (datetime.datetime.now().year+1)):
+    YEAR_CHOICES.append((r,r))
+
 class Player(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
@@ -12,35 +17,6 @@ class Player(models.Model):
     def __unicode__(self): 
         return u"%s, %s" % (self.last_name, self.first_name)
 
-class Team(models.Model):
-    team_name = models.CharField(max_length=30)
-    team_color = models.CharField(max_length=30)
-    is_active = models.BooleanField()
-
-    def __unicode__(self): 
-        return u"%s" % (self.team_name)
-
-class Roster(models.Model):
-    POSITION_TYPE = (
-    (1, 'Center'),
-    (2, 'Wing'),
-    (3, 'Defense'),
-    (4, 'Goalie')
-    )
-    player = models.ForeignKey(Player, related_name="+", null=True)
-    team = models.ForeignKey(Team)
-    position1 = models.PositiveIntegerField(choices=POSITION_TYPE)
-    position2 = models.PositiveIntegerField(choices=POSITION_TYPE)
-
-    def __unicode__(self): 
-        return u"%s: %s" % (self.team, str(self.player))
- # first_name = models.ForeignKey(Player, db_column='first_name', null=True, related_name="+")
- #    last_name = models.ForeignKey(Player, db_column='last_name', null=True, related_name="+")
-    
-YEAR_CHOICES = []
-for r in range(1980, (datetime.datetime.now().year+1)):
-    YEAR_CHOICES.append((r,r))
-
 class Season(models.Model):
     SEASON_TYPE = (
     (1, 'Spring'),
@@ -48,8 +24,6 @@ class Season(models.Model):
     (3, 'Fall'),
     (4, 'Winter')
     )
-    team = models.ForeignKey(Team)
-    is_champion = models.NullBooleanField()
     season_type = models.PositiveIntegerField(choices=SEASON_TYPE, null=True)
     year = models.IntegerField(choices=YEAR_CHOICES, default=datetime.datetime.now().year)
     is_current_season = models.NullBooleanField()
@@ -74,6 +48,32 @@ class League(models.Model):
 
     def __unicode__(self): 
         return u"%s: %s" % (self.season, self.get_division_display())
+
+class Team(models.Model):
+    team_name = models.CharField(max_length=30)
+    team_color = models.CharField(max_length=30)
+    league = models.ForeignKey(League, null=True)
+    is_active = models.BooleanField()
+
+    def __unicode__(self): 
+        return u"%s" % (self.team_name)
+
+class Roster(models.Model):
+    POSITION_TYPE = (
+    (1, 'Center'),
+    (2, 'Wing'),
+    (3, 'Defense'),
+    (4, 'Goalie')
+    )
+    player = models.ForeignKey(Player, related_name="+", null=True)
+    team = models.ForeignKey(Team)
+    position1 = models.PositiveIntegerField(choices=POSITION_TYPE)
+    position2 = models.PositiveIntegerField(choices=POSITION_TYPE, null=True, blank=True)
+
+    def __unicode__(self): 
+        return u"%s: %s" % (self.team, str(self.player))
+ # first_name = models.ForeignKey(Player, db_column='first_name', null=True, related_name="+")
+ #    last_name = models.ForeignKey(Player, db_column='last_name', null=True, related_name="+")
 
 class Game(models.Model):
     season = models.ForeignKey(Season)
