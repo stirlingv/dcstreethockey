61390b0f675504ab9fd74457efed46f2b66684e0
Stirling Van Winkle <stirling@Stirlings-MacBook-Pro.local>
2017-03-29 01:14:08 -0400

    adding team standings page

--- core/views.py
+++ core/views.py
@@ -13,13 +13,14 @@ from leagues.models import Season
 from leagues.models import MatchUp
 from leagues.models import Stat
 from leagues.models import Roster
+from leagues.models import Team_Stat
 # Create your views here.
 
 def home(request):
     context = {}
     # context["season"] = Season.objects.get(is_current_season=1)
     context["season"] = Season.objects.all()
-    context["matchup"]  = MatchUp.objects.filter(week__date__range=(datetime.date.today(), datetime.date.today() + timedelta(days=7)))
+    context["matchup"]  = MatchUp.objects.filter(week__date__range=(datetime.date.today(), datetime.date.today() + timedelta(days=9)))
 
     return render(request, "core/home.html", context=context)
 
@@ -41,3 +42,16 @@ class MatchUpDetailView(ListView):
 
         return context
 
+class TeamStatDetailView(ListView):
+    context_object_name = 'team_list'
+
+    def get_queryset(self):
+        return Team_Stat.objects.order_by('-win','loss','-tie')
+
+    def get_context_data(self, **kwargs):
+        context = super(TeamStatDetailView, self).get_context_data(**kwargs)
+        # context["season"] = Season.objects.all()
+        # context["roster"] = Roster.objects.order_by(Lower('player__last_name'))
+        # context["stat"] = Stat.objects.all()
+
+        return context
--- leagues/urls.py
+++ leagues/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from core.views import MatchUpDetailView
+from core.views import TeamStatDetailView
 
 urlpatterns = [
-	url(r'^$', MatchUpDetailView.as_view(), name='matchup-detail'),
+	url(r'^roster/$', MatchUpDetailView.as_view(), name='matchup-detail'),
+	url(r'^team_standings/$', TeamStatDetailView.as_view(), name='team-list'),
 ]
