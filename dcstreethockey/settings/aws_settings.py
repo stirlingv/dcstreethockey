e13281e835f196a28c1a9a350f0ba3abe4909e6a
Stirling Van Winkle <stirling@Stirlings-MacBook-Pro.local>
2017-09-18 22:34:26 -0400

    Adding S3 storage for media file perminances

---
+++ dcstreethockey/settings/aws_settings.py
@@ -0,0 +1,13 @@
+import os
+
+AWS_QUERYSTRING_AUTH = False
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME = os.environ['S3_BUCKET_NAME']
+MEDIA_URL = 'https://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto.S3BotoStorage"
+
+# S3DIRECT_ENDPOINT = 's3.amazonaws.com'  # http://docs.aws.amazon.com/general/latest/gr/rande.html#s3_region
+# S3DIRECT_DIR = 's3direct'  # (optional, default is 's3direct', location within the bucket to upload files)
+# S3DIRECT_UNIQUE_RENAME = False # (optional, default is 'False', gives the uploaded file a unique filename)
--- dcstreethockey/settings/production.py
+++ dcstreethockey/settings/production.py
@@ -1,4 +1,5 @@
 from .base import *
+from aws_settings import *
 import dj_database_url
 
 DATABASES = {'default': dj_database_url.config()}
