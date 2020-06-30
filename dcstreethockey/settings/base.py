2a457376accde979253bcc41af1ab9c3e1f64886
Stirling Van Winkle <stirling@Stirlings-MacBook-Pro.local>
2020-06-30 18:13:26 -0400

    adding static

--- dcstreethockey/settings/base.py
+++ dcstreethockey/settings/base.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.templatetags.static',
+    'django.contrib.staticfiles',
     'django_extensions',
     'core',
     'suit',
