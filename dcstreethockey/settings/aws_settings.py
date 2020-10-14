5177b8d0451e1bd019f25d71b30d8ba7798e9444
Stirling Van Winkle <stirling@Stirlings-MacBook-Pro.local>
2020-10-14 10:14:51 -0400

    udating static files storage for newer django version

--- dcstreethockey/settings/aws_settings.py
+++ dcstreethockey/settings/aws_settings.py
@@ -6,7 +6,7 @@ AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = os.environ['S3_BUCKET_NAME']
 MEDIA_URL = 'https://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
 
-DEFAULT_FILE_STORAGE = "storages.backends.s3boto.S3BotoStorage"
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # S3DIRECT_ENDPOINT = 's3.amazonaws.com'  # http://docs.aws.amazon.com/general/latest/gr/rande.html#s3_region
