697782cb89f674220048fadfa13846e43419b381
Stirling Van Winkle <stirling@Stirlings-MacBook-Pro.local>
2024-08-20 16:35:26 -0400

    making changes so logo shows up on every page for desktop

--- dcstreethockey/urls.py
+++ dcstreethockey/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('leagues/', include('leagues.urls', namespace='leagues')), 
     path('__debug__/', include(debug_toolbar.urls)),
-]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
 for url_pattern in get_resolver().url_patterns:
