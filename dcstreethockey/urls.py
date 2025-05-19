62e6f806962f5fb2399b873c29d7a22dde786377
Stirling Van Winkle <stirling@Stirlings-MacBook-Pro.local>
2025-05-19 14:45:35 -0400

    green jersey update and shortening URL to drop the need to inlude 'leagues' in the URL

--- dcstreethockey/urls.py
+++ dcstreethockey/urls.py
@@ -1,19 +1,42 @@
 from django.contrib import admin
-import core.views as core_view
 from django.conf.urls.static import static
 from django.conf import settings
+from django.urls import include, path, re_path
 import debug_toolbar
-from django.urls import include, path
-from django.urls import include, re_path
-from django.urls import get_resolver
+from core.views import (
+    MatchUpDetailView,
+    TeamStatDetailView,
+    PlayerStatDetailView,
+    PlayerAutocomplete,
+    home,
+    schedule,
+    teams,
+    scores,
+    cups,
+    PlayerAllTimeStats_list,
+    player_view,
+    player_trends_view,
+)
 
 urlpatterns = [
-    path('', core_view.home, name='home'),
-    path('admin/', admin.site.urls),
-    path('leagues/', include('leagues.urls')), 
-    path('__debug__/', include(debug_toolbar.urls)),
-]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('', home, name='home'),  # Home page
+    path('admin/', admin.site.urls),  # Admin panel
+    path('__debug__/', include(debug_toolbar.urls)),  # Debug toolbar
 
-
-for url_pattern in get_resolver().url_patterns:
-    print(url_pattern)
+    # Include core app URLs directly
+    path('roster/', MatchUpDetailView.as_view(), name='rosters'),
+    re_path(r'^roster/(?P<date>[0-9]{4}-[0-9]{2}-[0-9]{2})$', MatchUpDetailView.as_view(), name='rosters'),
+    path('team_standings/', TeamStatDetailView.as_view(), name='team_standings'),
+    path('player_stats/', PlayerStatDetailView.as_view(), name='player_stats'),
+    path('player_stats/<int:season>/', PlayerStatDetailView.as_view(), name='player_stats'),
+    path('teams/<int:team>/', teams, name='teams'),
+    path('player/<int:player_id>/', player_view, name='player'),
+    path('hof/', PlayerAllTimeStats_list, name='hof'),
+    path('schedule/', schedule, name='schedule'),
+    path('scores/', scores, name='scores'),
+    re_path(r'^scores/(?P<division>[0-9])/$', scores, name='scores'),
+    path('cups/', cups, name='cups'),
+    re_path(r'^cups/(?P<division>[0-9])/$', cups, name='cups'),
+    path('player_trends/', player_trends_view, name='player_trends'),
+    path('player-autocomplete/', PlayerAutocomplete.as_view(), name='player-autocomplete'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- leagues/urls.py
+++ leagues/urls.py
@@ -1,23 +1,4 @@
-from django.urls import path, re_path
-from core.views import MatchUpDetailView, TeamStatDetailView, PlayerStatDetailView, PlayerAutocomplete
-import core.views as core_view
+from django.urls import path
 
-urlpatterns = [
-    path('roster/', MatchUpDetailView.as_view(), name='rosters'),
-    re_path(r'^roster/(?P<date>[0-9]{4}-[0-9]{2}-[0-9]{2})$', MatchUpDetailView.as_view(), name='rosters'),
-    path('team_standings/', TeamStatDetailView.as_view(), name='team_standings'),
-    path('player_stats/', PlayerStatDetailView.as_view(), name='player_stats'),
-    path('player_stats/<int:season>/', PlayerStatDetailView.as_view(), name='player_stats'),
-    path('teams/<int:team>/', core_view.teams, name='teams'),
-    path('player/<int:player_id>/', core_view.player_view, name='player'),  # Updated to use player_id
-    path('hof/', core_view.PlayerAllTimeStats_list, name='hof'),
-    path('schedule/', core_view.schedule, name='schedule'),
-    path('scores/', core_view.scores, name='scores'),
-    re_path(r'^scores/(?P<division>[0-9])/$', core_view.scores, name='scores'),
-    path('cups/', core_view.cups, name='cups'),
-    re_path(r'^cups/(?P<division>[0-9])/$', core_view.cups, name='cups'),
-    path('player_trends/', core_view.player_trends_view, name='player_trends'),
-    path('player-autocomplete/', PlayerAutocomplete.as_view(), name='player-autocomplete'),
-]
-
-app_name = "leagues"
+# Placeholder for future app-specific URLs
+urlpatterns = []
