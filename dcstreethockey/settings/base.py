5b7391a74f68aae55864ee15966a3547bd68c4bc
Stirling Van Winkle <stirling@Stirlings-MacBook-Pro.local>
2025-05-19 16:48:13 -0400

    fixing staticfiles and url reference

--- dcstreethockey/settings/base.py
+++ dcstreethockey/settings/base.py
@@ -32,7 +32,7 @@ if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 ALLOWED_HOSTS = ['*']
 
